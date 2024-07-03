b2f7614a7c33af67f78234044cdadc0ee4bff2da
Bouha04 <armibh851@gmail.com>
2024-07-04 00:30:01 +0200

    made project

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
+
+const Hello = () => {
+  return (
+    <View>
+      <Text>
+        Hello, World ya ahmed mmtbga is delicious for real
+      </Text>
+    </View>
+  )
+}
+
+export default Hello;
