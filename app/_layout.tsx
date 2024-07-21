2426793654a317967ff885b3927038b76867f497
kbqcc <khaledbaccour@gmail.com>
2024-07-22 00:06:12 +0100

    yet another try to apply a custom font ya dinezzab

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,39 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    'Pix': require('../assets/fonts/Pixelify-Sans.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+    if (error) {
+      console.error('Error loading font:', error);
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <Stack>
-      <Stack.Screen name="index" options={
-        {
-          title: "Home",
-          headerShown: false
-        }
-      } />
-      <Stack.Screen name="loadingGame" options={
-        {
-          title: "loading",
-          headerShown: false
-        }
-      } />
-      <Stack.Screen name="game" options={
-        {
-          title: "game",
-          headerShown: false
-        }
-      } />
-      
+      <Stack.Screen name="index" options={{
+        title: "Home",
+        headerShown: false,
+      }} />
+      <Stack.Screen name="loadingGame" options={{
+        title: "Loading",
+        headerShown: false,
+      }} />
+      <Stack.Screen name="game" options={{
+        title: "Game",
+        headerShown: false,
+      }} />
     </Stack>
   );
 }
